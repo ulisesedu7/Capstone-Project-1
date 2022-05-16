3aa0de76127e058db05cd8fd0b17996b1f5d7d49
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-05-16 15:32:30 -0600

    Add first styles and changes to the heading section

---
+++ javascript/script.js
@@ -0,0 +1,33 @@
+/*
+JS Object Data
+*/
+// import projectInfo from './data.js';
+
+/*
+Hamburguer Menu constants
+*/
+const hamburger = document.querySelector('#nav-bar-btn');
+const navMenu = document.querySelector('.mobile-menu');
+const mainLogo = document.querySelector('#main-logo');
+const headerB = document.querySelector('#mobile-header');
+
+/*
+Open Hamburguer menu when the icon is clicked
+*/
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  mainLogo.classList.toggle('is-hidden');
+  headerB.classList.toggle('header-b');
+});
+
+/*
+Close Menu when you click the nav links
+*/
+document.querySelectorAll('.mobile-menu-a').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  mainLogo.classList.remove('is-hidden');
+  headerB.classList.remove('header-b');
+}));
+
