75bd8d0048d3eb8b2e8695ff50b7b56696ba8cc6
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-05-17 15:50:11 -0600

    Start Repertoire section that has to be dynamically created with js

---
+++ javascript/data.js
@@ -0,0 +1,44 @@
+const repertoireInfo = [{
+  id: 0,
+  featureImg: 'images/tchaikovski-img.jpg',
+  composerTitle: 'Pyotr Ilyich Tchaikovsky',
+  pieceName: 'Piano Concerto No.1 in B♭ minor, Op.23',
+  pieceDes: 'The Piano Concerto No. 1 in B♭ minor, Op. 23, was composed by Pyotr Ilyich Tchaikovsky between November 1874 and February 1875.',
+},
+{
+  id: 1,
+  featureImg: 'images/tchaikovski-img.jpg',
+  composerTitle: 'Pyotr Ilyich Tchaikovsky',
+  pieceName: 'Piano Concerto No.1 in B♭ minor, Op.23',
+  pieceDes: 'The Piano Concerto No. 1 in B♭ minor, Op. 23, was composed by Pyotr Ilyich Tchaikovsky between November 1874 and February 1875.',
+},
+{
+  id: 2,
+  featureImg: 'images/tchaikovski-img.jpg',
+  composerTitle: 'Pyotr Ilyich Tchaikovsky',
+  pieceName: 'Piano Concerto No.1 in B♭ minor, Op.23',
+  pieceDes: 'The Piano Concerto No. 1 in B♭ minor, Op. 23, was composed by Pyotr Ilyich Tchaikovsky between November 1874 and February 1875.',
+},
+{
+  id: 3,
+  featureImg: 'images/tchaikovski-img.jpg',
+  composerTitle: 'Pyotr Ilyich Tchaikovsky',
+  pieceName: 'Piano Concerto No.1 in B♭ minor, Op.23',
+  pieceDes: 'The Piano Concerto No. 1 in B♭ minor, Op. 23, was composed by Pyotr Ilyich Tchaikovsky between November 1874 and February 1875.',
+},
+{
+  id: 4,
+  featureImg: 'images/tchaikovski-img.jpg',
+  composerTitle: 'Pyotr Ilyich Tchaikovsky',
+  pieceName: 'Piano Concerto No.1 in B♭ minor, Op.23',
+  pieceDes: 'The Piano Concerto No. 1 in B♭ minor, Op. 23, was composed by Pyotr Ilyich Tchaikovsky between November 1874 and February 1875.',
+},
+{
+  id: 5,
+  featureImg: 'images/tchaikovski-img.jpg',
+  composerTitle: 'Pyotr Ilyich Tchaikovsky',
+  pieceName: 'Piano Concerto No.1 in B♭ minor, Op.23',
+  pieceDes: 'The Piano Concerto No. 1 in B♭ minor, Op. 23, was composed by Pyotr Ilyich Tchaikovsky between November 1874 and February 1875.',
+}];
+
+export default repertoireInfo;
